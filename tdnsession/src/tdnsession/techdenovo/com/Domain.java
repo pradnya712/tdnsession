01b33a7fa99fdc60214800c5841915014273e6fe
kalyanmazumder <kalyanmazumder@tdn-lpt01>
2019-11-07 13:12:56 +0530

    user input

---
+++ tdnsession/src/tdnsession/techdenovo/com/Domain.java
@@ -0,0 +1,26 @@
+package tdnsession.techdenovo.com;
+
+/**
+ * Admin
+ */
+public class Domain {
+
+    private String domainName;
+
+    public Domain(String domainName) {
+        this.domainName = domainName;
+    }
+
+    public Domain() {
+    }
+
+    public String getDomainName() {
+        return domainName;
+    }
+
+    public void setDomainName(String domainName) {
+        this.domainName = domainName;
+    }
+
+    
+}
---
+++ tdnsession/src/tdnsession/techdenovo/com/UserInput.java
@@ -0,0 +1,50 @@
+package tdnsession.techdenovo.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * UserInput
+ */
+public class UserInput {
+
+    public static void main(String[] args) {
+        String choice="";
+        Domain domain = new Domain();
+        while(true){
+            System.out.println("*******************");
+            System.out.println("Enter option No.");
+            System.out.println("A. Administration");
+            System.out.println("U. User");
+            System.out.println("E. Exit");
+            System.out.println("*******************");
+            try {
+                choice = new BufferedReader(new InputStreamReader(System.in)).readLine();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            
+            switch (choice.toUpperCase()) {
+                case "A":
+                    System.out.println("Domain Entry/Update");
+                    System.out.println("Enter Domain Name: ");
+                    try {
+                        domain.setDomainName( new BufferedReader(new InputStreamReader(System.in)).readLine());
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    
+                    break;
+                case "U":
+                    System.out.println("U. User");
+                    break;
+                case "E":
+                    System.out.println("E.Exit");
+                    System.exit(1);
+                default:
+                    break;
+            }
+            System.out.println(domain.getDomainName());
+        }
+    }
+}
